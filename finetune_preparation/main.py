a39764c557d41137bf54ae69c5aa981c8695bd19
ririye01 <ririye01@gmail.com>
2024-04-08 20:15:24 -0500

    Now generates 8 images per prompt

--- finetune_preparation/main.py
+++ finetune_preparation/main.py
@@ -7,7 +7,8 @@ from dotenv import load_dotenv
 
 def main() -> None:
     # Define batch size
-    BATCH_SIZE = 300
+    BATCH_SIZE = 50
+    PROMPT_ITERATION_COUNT = 8
 
     # Load environment variables (e.g. HuggingFace API Key)
     load_dotenv()
@@ -31,16 +32,21 @@ def main() -> None:
         end_idx = min(start_idx + BATCH_SIZE, total_prompts)
         batch_prompts = all_prompts[start_idx:end_idx]
 
+        # Indicate starting point for batch image metadata
+        all_batch_image_metadata = []
+
         # Generate images for the current batch
-        print(f"Generating batch {batch_num + 1}/{num_batches}...")
-        batch_image_metadata = (
-            generate.generate_lora_input_images_and_associated_metadata(batch_prompts, pipeline)
-        )
+        for prompt_iteration in range(1, PROMPT_ITERATION_COUNT+1):
+            print(f"Generating batch {batch_num + 1}/{num_batches} with prompt iteration {prompt_iteration}/{PROMPT_ITERATION_COUNT}...")
+            batch_image_metadata = (
+                generate.generate_lora_input_images_and_associated_metadata(batch_prompts, pipeline)
+            )
+            all_batch_image_metadata.extend(batch_image_metadata)
 
         # Convert the batch's images to a Parquet file and push to Hugging Face
         parquet_file_name = f"lora-input-data-batch-{batch_num + 1}.parquet"
         hf.convert_images_to_parquet_and_push(
-            all_image_metadata=batch_image_metadata,
+            all_image_metadata=all_batch_image_metadata,
             parquet_file_name=parquet_file_name,
             dataset_repo=dataset_repo,
         )
