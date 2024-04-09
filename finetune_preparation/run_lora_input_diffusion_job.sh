3254a62b6867f887dce3ebb0bd14dec950317ffd
Reece D Iriye <ririye01@gmail.com>
2024-04-08 21:24:26 -0500

    Removed --exclusive and lowered memory requirement

--- finetune_preparation/run_lora_input_diffusion_job.sh
+++ finetune_preparation/run_lora_input_diffusion_job.sh
@@ -2,12 +2,11 @@
 
 #SBATCH -J lora-input-diffusion     # Job Name
 #SBATCH -p batch                    # (debug or batch)
-#SBATCH --exclusive
 #SBATCH -o lora-input-diffusion.out # Output file name
 #SBATCH --gres=gpu:1                # Number of GPUs to use
 #SBATCH --mail-user ririye@smu.edu
 #SBATCH --mail-type=ALL
-#SBATCH --mem=300GB
+#SBATCH --mem=70GB
 
 pip install -r ../requirements.txt
 python main.py
