3cf994be20f571c848c3c6d96f43ac002e38d4da
Reece D Iriye <ririye01@gmail.com>
2024-04-09 21:51:57 -0500

    Swapped job script to 128GB for SD-XL Finetune

--- finetune_preparation/run_lora_input_diffusion_job.sh
+++ finetune_preparation/run_lora_input_diffusion_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1                # Number of GPUs to use
 #SBATCH --mail-user ririye@smu.edu
 #SBATCH --mail-type=ALL
-#SBATCH --mem=70GB
+#SBATCH --mem=128GB
 
 pip install -r ../requirements.txt
 python main.py
