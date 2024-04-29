a3aa73fdb85005e3989f7c611f79324b9a17fc61
TrevorDohm <tdohm@smu.edu>
2024-04-29 00:08:37 -0500

    Added Kohya Folder Creation

--- 2_lora_training/unpack.py
+++ 2_lora_training/unpack.py
@@ -2,31 +2,26 @@
 import os
 import pandas as pd
 
-# Directory Containing Parquets
-directory = "../../Generated-LoRA-Input-Images-for-Mitigating-Bias"
+# Directory Containing Parquets (Swap Base For ../../Benchmark-Images-for-Stable-Diffusion-Bias)
+base_directory = "../../Generated-LoRA-Input-Images-for-Mitigating-Bias"
+os.makedirs(os.path.join(base_directory, "log"), exist_ok = True)
+os.makedirs(os.path.join(base_directory, "model"), exist_ok = True)
 
 # Target Directory For Saving Images, Captions
-target_directory = directory + "/img/10_race/"
+target_directory = os.path.join(base_directory, "img/10_race/")
 os.makedirs(target_directory, exist_ok = True)
 
 # List All Parquet Files In Directory
-parquet_files = [f for f in os.listdir(directory) if f.endswith(".parquet")]
+parquet_files = [f for f in os.listdir(base_directory) if f.endswith(".parquet")]
 
 # Load Each Parquet File, Concat
-df = pd.concat(
-    (pd.read_parquet(os.path.join(directory, pf)) for pf in parquet_files),
-    ignore_index = True,
-)
+df = pd.concat((pd.read_parquet(os.path.join(base_directory, pf)) for pf in parquet_files), ignore_index = True)
 
-# Iterate Over DataFrame Rows
+# Iterate Over DataFrame Rows, Save Information
 for index, row in df.iterrows():
     image_file_path = os.path.join(target_directory, f"{index + 1}.png")
     caption_file_path = os.path.join(target_directory, f"{index + 1}.txt")
-
-    # Save Image Data
     with open(image_file_path, 'wb') as img_file:
         img_file.write(row["image"])
-
-    # Save Prompts
     with open(caption_file_path, 'w') as txt_file:
         txt_file.write(row["prompt"])
