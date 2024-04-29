43631ab5a9e1d910a80a8015f128825d0c1ef46c
Reece D Iriye <ririye01@gmail.com>
2024-04-29 15:52:06 -0500

    Upload shell script for SuperPOD

---
+++ 0_benchmark_analysis_and_problem_identification/benchmark_analysis/upload_attributes/upload_face_attributes.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#SBATCH -J face-attributes-upload     # Job Name
+#SBATCH -p batch                    # (debug or batch)
+#SBATCH -o upload_log.out           # Output file name
+#SBATCH --gres=gpu:1                # Number of GPUs to use
+#SBATCH --mail-user ririye@smu.edu
+#SBATCH --mail-type=ALL
+#SBATCH --mem=128GB
+
+python main.py
