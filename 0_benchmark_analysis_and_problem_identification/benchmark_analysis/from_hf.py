63f3ab01894ac4abdb9676cf5deb096c2873e774
Reece D Iriye <ririye01@gmail.com>
2024-04-29 23:27:27 -0500

    Not keeping load dataset in memory

--- 0_benchmark_analysis_and_problem_identification/benchmark_analysis/from_hf.py
+++ 0_benchmark_analysis_and_problem_identification/benchmark_analysis/from_hf.py
@@ -1,9 +1,10 @@
 from datasets import load_dataset
 from datasets.dataset_dict import Dataset
 
+
 def get_data_from_huggingface(
     dataset_repo: str = "ririye/Benchmark-Images-for-Stable-Diffusion-Bias",
 ) -> Dataset:
-    dataset = load_dataset(dataset_repo, split="train")
+    dataset = load_dataset(dataset_repo, split="train", keep_in_memory=False)
     assert isinstance(dataset, Dataset)
     return dataset
